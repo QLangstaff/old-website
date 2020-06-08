411061a7a86433914b80291b2ccb3e3e17e9f1e8
QLangstaff <64713281+QLangstaff@users.noreply.github.com>
2020-06-08 18:50:03 -0400

    Update and rename contact-form-handler.php to send.php

---
+++ send.php
@@ -0,0 +1,19 @@
+Thanks for your message!
+
+<?php
+
+    $userName = $_POST['name'];
+    $userEmail = $_POST['email'];
+    $userMessage = $_POST['message'];
+
+    $to = "qali.langstaff@gmail.com";
+    $subject = "Message from qali.me";
+    $body = "Information submitted: ";
+
+
+    $body .= "\r\n Name: " . $userName;
+    $body .= "\r\n Email: " . $userEmail;
+    $body .= "\r\n Message: " . $userMessage;
+
+    mail($to, $subject, $body);
+?>
